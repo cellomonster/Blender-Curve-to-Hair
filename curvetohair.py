a383f7f82470667ac9abff2aaeb0a3e56c41cdb5
cellomonster <triveri@outlook.com>
2021-10-11 12:07:57 -0400

    implement curve rescale to support child hairs

--- curvetohair.py
+++ curvetohair.py
@@ -45,11 +45,9 @@ def main(context):
 		else:
 			continue
 		
-		#flip spline tilt
-		#note: for some reason, spline tilt twists hair in the opposite direction from what you'd expect
-		#this is the only way I can think to fix the issue :(
-		for spline_point in spline_points:
-				spline_point.tilt = -spline_point.tilt
+		#create a collection for the field to influence and add the curve to it 
+		field_collection = bpy.context.blend_data.collections.new(name='curve to hair influence col')
+		field_collection.use_fake_user = True
 			
 		#calculate rotation of the hair emitter
 		hair_emitter_normal = mathutils.Vector(spline_points[0].co.xyz - spline_points[1].co.xyz).normalized()
@@ -70,17 +68,46 @@ def main(context):
 		curve_object.field.type = 'GUIDE'
 		curve_object.field.guide_minimum = 0
 		
-		#create a collection for the field to influence and add the curve to it 
-		field_collection = bpy.context.blend_data.collections.new(name='curve to hair influence col')
-		field_collection.objects.link(curve_object)
-		field_collection.use_fake_user = True
+		#rescale curve so that first spline point radius is 1
 		
+		print("spline_points length: " + str(len(spline_points)))
+		
+		bpy.ops.object.select_all(action='DESELECT')
+		bpy.context.view_layer.objects.active = curve_object
+		bpy.ops.view3d.snap_cursor_to_selected()
+		previous_piviot_mode = bpy.context.scene.tool_settings.transform_pivot_point
+		bpy.context.scene.tool_settings.transform_pivot_point = 'CURSOR'
+		
+		scale_inverse = 1 / spline_points[0].radius
+		scale = spline_points[0].radius
+		bpy.ops.object.editmode_toggle()
+		bpy.ops.curve.select_all(action='SELECT')
+		bpy.ops.transform.resize(value = (scale_inverse, scale_inverse, scale_inverse), orient_type='GLOBAL')
+		bpy.ops.transform.transform(mode='CURVE_SHRINKFATTEN', value = (scale_inverse, 0, 0, 0), orient_type='GLOBAL')
+		bpy.ops.object.editmode_toggle()
+		bpy.ops.transform.resize(value = (scale, scale, scale), orient_type='GLOBAL')
+		bpy.context.scene.tool_settings.transform_pivot_point = previous_piviot_mode
+		
+		#get spline and points again after editing
+		#not sure why this is needed
+		spline = curve_object.data.splines[0]
+		if spline.type == 'BEZIER':
+			spline_points = spline.bezier_points
+		elif spline.type == 'NURBS':
+			spline_points = spline.points
+		
+		for spline_point in spline_points:
+			spline_point.tilt = -spline_point.tilt
+			
+		field_collection.objects.link(curve_object)
+		
+		#create hair emitter
 		hair_emitter = None
 			
 		#todo: avoid using bpy.ops
 		if curve_data.bevel_mode == 'ROUND' :	
 			#create circle with radius of round bevel curve
-			bpy.ops.mesh.primitive_circle_add(fill_type='TRIFAN', radius = curve_data.bevel_depth, rotation = hair_emitter_rotation.to_euler(), location = (0, 0, 0))
+			bpy.ops.mesh.primitive_circle_add(fill_type='TRIFAN', radius = curve_data.bevel_depth, location = (0, 0, 0))
 			#the newly created circle is selected, so grab it from context
 			hair_emitter = bpy.context.active_object
 			
@@ -107,8 +134,8 @@ def main(context):
 		hair_emitter.location = (0, 0, 0)
 		#size emitter depending on radius
 		radius_inverse = 1 / spline_points[0].radius
-		hair_emitter.scale = (radius_inverse, radius_inverse, radius_inverse)
-		bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
+		#hair_emitter.scale = (radius_inverse, radius_inverse, radius_inverse)
+		#bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
 		#add hair
 		bpy.ops.object.particle_system_add()
 		hair_settings = hair_emitter.particle_systems[0].settings
@@ -151,14 +178,14 @@ def context_menu_func(self, context):
 
 def register():
 	bpy.utils.register_class(CurveToHair)
-	bpy.types.VIEW3D_MT_object_convert.append(menu_func)
-	bpy.types.VIEW3D_MT_object_context_menu.append(context_menu_func)
+#	bpy.types.VIEW3D_MT_object_convert.append(menu_func)
+#	bpy.types.VIEW3D_MT_object_context_menu.append(context_menu_func)
 
 
 def unregister():
 	bpy.utils.unregister_class(CurveToHair)
-	bpy.types.VIEW3D_MT_object_convert.remove(menu_func)
-	bpy.types.VIEW3D_MT_object_context_menu.remove(context_menu_func)
+#	bpy.types.VIEW3D_MT_object_convert.remove(menu_func)
+#	bpy.types.VIEW3D_MT_object_context_menu.remove(context_menu_func)
 
 
 if __name__ == "__main__":
