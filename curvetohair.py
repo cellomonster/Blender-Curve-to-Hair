80d07b7b6a09e1002642b0aaa482d1758a5b415c
cellomonster <triveri@outlook.com>
2021-10-12 14:54:18 -0400

    clean up code

--- curvetohair.py
+++ curvetohair.py
@@ -19,48 +19,56 @@ from math import radians
 
 def main(context):
 	for curve_object in bpy.context.selected_objects:
-		#check that it is a curve
+		#check that selected object is a curve
 		if curve_object.type != 'CURVE':
 			continue
 		
 		curve_data = curve_object.data
 		
-		#check bevel mode. profile is not supported (yet:tm:)
+		#check bevel mode. profile is not supported (yet)
+		#todo: support profile mode
 		if curve_data.bevel_mode == 'PROFILE':
-			#todo: support profile mode
 			continue
 		
-		#make sure the curve actually has a bevel object
+		#if ROUND, check that round bevel curve has depth
+		if curve_data.bevel_mode == 'ROUND' and curve_data.bevel_depth == 0:
+			continue
+		
+		#if OBJECT, make sure the curve actually has a bevel object
 		if curve_data.bevel_mode == 'OBJECT' and curve_data.bevel_object == None:
 			continue
 		
 		spline = curve_data.splines[0]
 		spline_points = None
 		
-		hair_emitter_normal = None
 		is_bezier = False
-		
+		spline_p0 = None
+		pos_2 = None
+		#calculate rotation of the hair emitter
 		#points are different depending on if the curve is a NURBS or bezier curve
 		if spline.type == 'BEZIER':
 			spline_points = spline.bezier_points
-			hair_emitter_normal = mathutils.Vector(spline_points[0].co - spline_points[0].handle_right).normalized()
+			pos_2 = spline_points[0].handle_right
 			is_bezier = True
 		elif spline.type == 'NURBS':
 			spline_points = spline.points
-			hair_emitter_normal = mathutils.Vector(spline_points[0].co.xyz - spline_points[1].co.xyz).normalized()
+			pos_2 = spline_points[1].co.xyz
 		else:
 			continue
 		
-		curve_data.use_path = True
+		spline_p0 = spline_points[0]
 		
 		#calculate rotation of the hair emitter
-		hair_emitter_rotation = hair_emitter_normal.to_track_quat('Z', 'Y')
+		hair_em_norm = mathutils.Vector(spline_p0.co.xyz - pos_2)
+		hair_emitter_rotation = hair_em_norm.to_track_quat('Z', 'Y')
 		
 		#create a collection for the field to influence and add the curve to it
-		field_collection = bpy.context.blend_data.collections.new(name='curve to hair influence col')
+		field_collection = bpy.context.blend_data.collections.new(name='curve to hair col')
 		field_collection.use_fake_user = True
 		field_collection.objects.link(curve_object)
 		
+		#required
+		curve_data.use_path = True
 		#make curve wireframe in viewport
 		curve_object.display_type = 'WIRE'
 		curve_object.hide_render = True
@@ -77,57 +85,60 @@ def main(context):
 		curve_object.field.guide_minimum = 0
 		
 		#rescale curve so that first spline point radius is 1
-		scale = spline_points[0].radius
+		#this is needed for child hairs to work as expected
+		scale = spline_p0.radius
 		curve_origin = curve_object.location
 		for spline_point in spline_points:
 			spline_point.co /= scale
+			#bezier point handles must also be moved
 			if is_bezier:
 				spline_point.handle_right /= scale
 				spline_point.handle_left /= scale
 			spline_point.radius /= scale
 			#tilt is flipped as hairs twist opposite of tilt
-			#dont ask me why
 			spline_point.tilt = -spline_point.tilt
 		curve_object.scale *= scale
 		
 		#create hair emitter
 		hair_emitter = None
-			
-		#todo: avoid using bpy.ops
+		#if ROUND, the emitter is a disk matching the circle resolution of the bevel
 		if curve_data.bevel_mode == 'ROUND' :
-			hair_emitter_mesh = bpy.data.meshes.new('hair emitter mesh')
-			hair_emitter = bpy.data.objects.new('hair emitter', hair_emitter_mesh)
+			mesh = bpy.data.meshes.new('hair emitter mesh')
+			hair_emitter = bpy.data.objects.new('hair emitter', mesh)
 			#create circle with radius of round bevel curve
-			#bpy.ops.mesh.primitive_circle_add(fill_type='TRIFAN', radius = curve_data.bevel_depth, location = (0, 0, 0))
-			#the newly created circle is selected, so grab it from context
 			bm = bmesh.new()
 			bm.from_mesh(hair_emitter.data)
-			bmesh.ops.create_circle(bm, cap_ends = True, cap_tris = True, segments = curve_data.bevel_resolution + 4, radius = curve_data.bevel_depth)
+			bmesh.ops.create_circle(bm, 
+			                        cap_ends = True, 
+			                        cap_tris = True, 
+			                        segments = curve_data.bevel_resolution + 4, 
+			                        radius = curve_data.bevel_depth)
 			bm.to_mesh(hair_emitter.data)
 			bm.free()
-
 			
-			#hair_emitter = bpy.context.active_object
-			
+		#if BEVEL, create a new object with a mesh converted from the bevel object curve
 		elif curve_data.bevel_mode == 'OBJECT' :
 			#create a mesh version of the bevel object
 			depsgraph = bpy.context.evaluated_depsgraph_get()
 			object_eval = curve_data.bevel_object.evaluated_get(depsgraph)
-			tmpMesh = bpy.data.meshes.new_from_object(object_eval)    
-			hair_emitter = bpy.data.objects.new(name='hair emitter', object_data = tmpMesh)
+			tmp_mesh = bpy.data.meshes.new_from_object(object_eval)    
+			hair_emitter = bpy.data.objects.new(name='hair emitter', object_data = tmp_mesh)
 			bm = bmesh.new()
 			bm.from_mesh(hair_emitter.data)
+			#create face from verts
 			bmesh.ops.holes_fill(bm, edges = bm.edges, sides = len(bm.edges))
 			bm.to_mesh(hair_emitter.data)
 			bm.free()
 			#todo apply scale after scaling hair_emitter
 			hair_emitter.scale = curve_data.bevel_object.scale
 			
-		#orient emitter
+		#link to view layer collection so the hairs are visible 
 		bpy.context.view_layer.layer_collection.collection.objects.link(hair_emitter)
+		
+		#orient emitter
 		hair_emitter.parent = curve_object
 		hair_emitter.rotation_euler = hair_emitter_rotation.to_euler()
-		hair_emitter.rotation_euler.rotate_axis('Z', spline_points[0].tilt)
+		hair_emitter.rotation_euler.rotate_axis('Z', spline_p0.tilt)
 		hair_emitter.location = (0, 0, 0)
 		#add hair
 		hair_emitter.modifiers.new("part", type='PARTICLE_SYSTEM')
@@ -143,7 +154,8 @@ def main(context):
 		hair_emitter.show_instancer_for_viewport = False
 		
 		if len(curve_object.material_slots) > 0:
-			hair_emitter.data.materials.append(curve_object.material_slots[0].material)
+			curve_mat = curve_object.material_slots[0].material
+			hair_emitter.data.materials.append(curve_mat)
 
 class CurveToHair(bpy.types.Operator):
 	"""Tooltip"""
